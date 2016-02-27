5bbf3530a0dd3b7b4f73b0483137530c75bb3791
Gustavo Iha <gustavoiha@gmail.com>
2016-02-27 19:00:58 -0300

    Conversion to NodeJs

---
+++ Constants.js
@@ -0,0 +1,19 @@
+
+/**
+* File with all the constants in the programs.
+*/
+
+// Physical constants.
+exports.AIR_SPEED     = 300;          //**** m/s
+exports.ROCKET_MASS   = 2000;         //**** kg
+exports.EARTH_RATIO   = 6371000;      // m
+exports.GRAVITATIONAL = 6.67384e-11;  // m^2/s^2
+
+//Distance from the CG and CP to the rocket's nose.
+exports.CG_DISTANCE = 0.5;            //**** m
+exports.CP_DISTANCE = 0.7;            //**** m
+
+//Inertial Moments.
+exports.I_x = 33730861.08e3;          //**** kg*m^2
+exports.I_y = 13826446202.68e3;       //**** kg*m^2
+exports.I_z = 13826555274.94e3;       //**** kg*m^2
