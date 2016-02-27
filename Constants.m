92b3128c361c070f746bd9eced5efd049f8f8711
Bitencs <gabriel.bitencs@gmail.com>
2016-02-27 12:21:04 -0300

    Finished defining the physical constants. Some values are not correct yet.

--- Constants.m
+++ Constants.m
@@ -1,17 +1,27 @@
 
-%%6DoF Simulation 
+%6DoF Simulation
 %Authors: Gabriel Bitencourt de A. Moura, Gustavo Iha R. Moraes
-%Version 1.0.0
+%Version 1.0.1
 %Atualizations: 1.Building a class of constants
 
 classdef Constants
     properties (Constant)
-        AIR_SPEED = 300          %**** m/s
-        ROCKET_MASS = 2000       %**** kg
-        EARTH_RATIO = 6371000;   % m
-        GM = 6.67384e-11;       % m^2/s^2
+        AIR_SPEED       = 300               %**** m/s
+        ROCKET_MASS     = 2000              %**** kg
+        EARTH_RATIO     = 6371000;          % m
+        GRAVITATIONAL   = 6.67384e-11;      % m^2/s^2
+        
+    %Distance from the CG and CP to the rocket's nose.
+        CG_DISTANCE     = 0.5               %**** m
+        CP_DISTANCE     = 0.7               %**** m 
+    
+    %Inertial Moments.
+        I_x             = 33730861.08e3     %**** kg*m^2
+        I_y             = 13826446202.68e3  %**** kg*m^2
+        I_z             = 13826555274.94e3  %**** kg*m^2 
+  
     end
 end
 
 %Caption:
-    %**** it menas that isn't the right value yet.
+    %**** means that isn't the right value yet.
