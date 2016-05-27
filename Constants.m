954b9556c8a9dc71863fd72d0304f4a466a0e136
Bitencs <gabriel.bitencs@gmail.com>
2016-05-26 22:13:18 -0300

    Change the programming language and the constants file

---
+++ Constants.m
@@ -0,0 +1,17 @@
+
+%%6DoF Simulation 
+%Authors: Gabriel Bitencourt de A. Moura, Gustavo Iha R. Moraes
+%Version 1.0.0
+%Atualizations: 1.Building a class of constants
+
+classdef Constants
+    properties (Constant)
+        AIR_SPEED = 300             %**** m/s
+        ROCKET_MASS = 2000          %**** kg
+        EARTH_RATIO = 6371000;      % m
+        GM = 6.67384e-11;           % m^2/s^2
+    end
+end
+
+%Caption:
+    %**** it menas that isn't the right value yet.
