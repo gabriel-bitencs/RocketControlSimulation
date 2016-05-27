56647eaf5d3282e7ebac93602c55a155d25b02f2
Gustavo Iha <gustavoiha@gmail.com>
2016-05-27 17:21:27 -0300

    Changed a few constant names

--- BarrowmanCP.m
+++ BarrowmanCP.m
@@ -10,14 +10,14 @@ classdef BarrowmanCP
         OGIVE_BASE_DIAMETER = 12.5*10^(-3);                     % m
         
         %% Transmission
-        F_TRANSMISSION_DIAMETER = 12.5*10^(-3);                 % Diameter at Front of Transmission (m)
-        R_TRANSMITION_DIAMETER = 2*50.80*10^(-3);               % Diameter at Rear of Transmission  (m)
+        TRANSMISSION_DIAMETER_FRONT = 12.5*10^(-3);             % Diameter at Front of Transmission (m)
+        TRANSMITION_DIAMETER_REAR = 2*50.80*10^(-3);            % Diameter at Rear of Transmission  (m)
         TRANSMISSION_LENGHT = 100*10^(-3);                  
         
         %% Distances
         DISTANCE_MID_CHORD = 1640*10^(-3) - 270*10^(-3);        % Distance between fin root leading edge and fin tip leading edge parallel to body
         DISTANCE_TIP_FIN = 85*10^(-3);                          % Distance from nose Tip to Fin root chord leading edge
-        DISTANCE_TIP_FTRANSMISSION = 640*10^(-3);               % Distance from nose tip to front of Transmission
+        DISTANCE_TIP_TRANSMISSION_FRONT = 640*10^(-3);          % Distance from nose tip to front of Transmission
         
         %% Fins
         FINS = 4;                                               % Number of fins
