3cb60fc9b96ca4121ccdf5340d90076f393f6e1f
Gustavo Iha <gustavoiha@gmail.com>
2016-05-27 14:13:09 -0300

    Added atmosphere and drag constants. Fixed typos

--- SimulatorConstants.m
+++ SimulatorConstants.m
@@ -1,36 +1,47 @@
 
-%% 6DoF Simulation 
+%% 6DoF Simulation
     %Authors: Gabriel Bitencourt de A. Moura, Gustavo Iha R. Moraes
     %Version 1.0.0
     %Atualizations: 1.Building a class of constants
-    
+
 %% Initially, this class has all the main constants used in the simulator.
 classdef SimulatorConstants
-    properties (Constant)      
+    properties (Constant)
+
         %% Propelent data
-        PROPELENT_INICIAL_MASS = 3000;                         %**** kg
-        PROPELENT_BURN = 30000;                               %**** s
-        
+        PROPELENT_INICIAL_MASS = 3000;                          %**** kg
+        PROPELENT_BURN_DURATION = 30000;                        %**** s
+
         %% Rocket data
-        ROCKET_MASS = 2000;                                    %**** kg
-        ROCKET_THRUST = 3000;                                  %**** kg*m/s^2
-        ROCKET_LATERAL_AREA = 200;                             %**** m?
-        ROCKET_FRONTAL_AREA = 100;                             %**** m?
+        ROCKET_DRY_MASS = 2000;                                 %**** kg
+        ROCKET_THRUST = 3000;                                   %**** kg*m/s^2
+        ROCKET_LATERAL_AREA = 200;                              %**** m^2
+        ROCKET_FRONTAL_AREA = 100;                              %**** m^2
+
+        %% Rocket's Moment of Inercia around the three axes
+        ROCKET_INERCIA_MOMENT_Z = 33730861.08    * 1000;        %**** kg * m^2
+        ROCKET_INERCIA_MOMENT_X = 13826446202.68 * 1000;        %**** kg * m^2
+        ROCKET_INERCIA_MOMENT_Y = 13826555274.94 * 1000;        %**** kg * m^2
+
+        %% Rocket's drag coefficients
+        ROCKET_DRAG_COEFFICIENT_FRONTAL = 0.45;
+        ROCKET_DRAG_COEFFICIENT_LATERAL = 0.45;
         
-        %% Rocket Moment of Inercia around the three axes
-        ROCKET_INERCIA_MOMENTz = 33730861.08    * 1000;        %**** kg * m^2
-        ROCKET_INERCIA_MOMENTx = 13826446202.68 * 1000;        %**** kg * m^2
-        ROCKET_INERCIA_MOMENTy = 13826555274.94 * 1000;        %**** kg * m^2
-        
+        %% ATMOSPHERE
+        AIR_DENSITY = 1.2922                                    %**** kg/m^3
+        WIND_SPEED_X = 0;                                       %**** m/s
+        WIND_SPEED_Y = 0;                                       %**** m/s
+        WIND_SPEED_Z = 0;                                       %**** m/s
+
         %% Others
-        EARTH_RATIO = 6371000;                                 % m
-        EARTH_MASS = 5.972e24;                                 % kg
-        GM = 6.67384e-11;                                      % m^3/kg/s^2
-        AIR_SPEED = 300;                                       %**** m/s
-        DISTANCE_CP_CG = 463782                                %**** m
-        
+        EARTH_RATIO = 6371000;                                  % m
+        EARTH_MASS = 5.972e24;                                  % kg
+        GRAVITATIONAL_CONSTANT = 6.67384e-11;                   % m^3/kg/s^2
+        AIR_SPEED = 300;                                        %**** m/s
+        DISTANCE_CP_CG = 463782                                 %**** m
+
     end
 end
 
-        %Caption: ****the values above aren't define yet. For now, we are
+        %Caption: ****the values above aren't defined yet. For now, we are
         %using UFABCRocket's values.
