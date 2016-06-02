49fddb7fabc4e962498c255b932efc4c60f8534a
Gustavo Iha <gustavoiha@gmail.com>
2016-06-02 17:59:59 -0300

    Fixed typos

--- RocketInitialConditions.m
+++ RocketInitialConditions.m
@@ -2,19 +2,18 @@
 % Authors: Gabriel Bitencourt de A. Moura, Gustavo Iha R. Moraes
 % Version 1.0.0
 
-%% Rocket Incial Conditions
-classdef RocketIntialConditions
+%% Rocket Initial Conditions
+classdef RocketInitialConditions
     properties
         Rocket_Position = [0,0,0];
         Rocket_LinearVelocity = [0,0,0];
         Rocket_AngularVelocity = [0,0,0];
         Rocket_LinearAcceleration = [0,0,0];
         Rocket_AngularAcceleration = [0,0,0];
-        
     end   
     methods (Static)
-        function IntialCondintions = RocketIntialCondintions()
-            IntialCondintions = {Rocket_LinearVelocity; Rocket_AngularVelocity; Rocket_LinearAcceleration; Rocket_AngularAcceleration};
+        function InitialConditions = RocketInitialConditions()
+            InitialConditions = {Rocket_LinearVelocity; Rocket_AngularVelocity; Rocket_LinearAcceleration; Rocket_AngularAcceleration};
         end
     end
     
