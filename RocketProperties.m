110d1f342dc3594dbe1a4672d51468b823a2c51e
Gustavo Iha <gustavoiha@gmail.com>
2016-06-03 00:02:33 -0300

    Fixed typos and bugs

--- RocketProperties.m
+++ RocketProperties.m
@@ -2,20 +2,20 @@
 % Authors: Gabriel Bitencourt de A. Moura, Gustavo Iha R. Moraes
 % Version 1.0.0
 
-%% Rocket Incial Conditions
+%% Rocket's Initial Conditions
 classdef RocketProperties
     
     properties 
         %% Physical 
         Position = [0,0,0];                      %[x,y,z] m
         LinearVelocity = [0,0,0];                %[u,v,w] m/s
-        AngularVelocity = [0,0,0];               %[P,Q, R] rad/s 
+        AngularVelocity = [0,0,0];               %[P,Q,R] rad/s 
         LinearAcceleration = [0,0,0];            %[u',v',w'] m/s^2
         AngularAcceleration = [0,0,0];           %[P',Q',R'] rad/s^2
         
-        %% Euler's Angles (in degress) 
+        %% Euler's Angles (in degrees) 
         Phi = 0;                                                     
-        Psi = 0
+        Psi = 0;
         Theta = 0;
         
     end  
@@ -30,7 +30,7 @@ classdef RocketProperties
             q_2 = sin(obj.Theta/2)*cos(obj.Psi/2)*cos(obj.Phi/2) - sin(obj.Psi/2)*sin(obj.Psi/2)*cos(obj.Theta/2);
             q_3 = sin(obj.Phi/2)*cos(obj.Psi/2)*cos(obj.Theta/2) + sin(obj.Phi/2)*sin(obj.Theta/2)*cos(obj.Psi/2);
             
-            %% Creates the quarternion vector and determinates it's norm
+            %% Creates the quarternion vector and determines its norm
             QuaternionVector = [q_0, q_1, q_2, q_3];
             QuaternionNorm = norm(QuaternionVector);
             
@@ -46,10 +46,10 @@ classdef RocketProperties
     end
     
     methods
-        function TransformMatrix = Calcuate_TransformMatrix(Quaternion)
+        function TransformMatrix = CalculateTransformMatrix(~,Quaternion)
              % Generates Transform Matrix from the quaternions' vector
              
-            %% Quantifies terms of Transform Tarix
+            %% Quantifies terms of Transform Matrix
             a11 = Quaternion(1)^2 + Quaternion(2)^2 - Quaternion(3)^2 - Quaternion(4)^2;
             a12 = 2*(Quaternion(2)*Quaternion(3) - Quaternion(1)*Quaternion(4));
             a13 = 2*(Quaternion(2)*Quaternion(3) + Quaternion(1)*Quaternion(3));
@@ -60,7 +60,7 @@ classdef RocketProperties
             a32 = 2*(Quaternion(3)*Quaternion(4) + Quaternion(1)*Quaternion(2));
             a33 = Quaternion(1)^2 - Quaternion(2)^2 - Quaternion(3)^2 + Quaternion(4)^2;
             
-            %% Building and returning Transform Matrix
+            %% Building and returning the Transform Matrix
             TransformMatrix = [a11 a12 a13; a21 a22 a23; a31 a32 a33];            
         end
     end
